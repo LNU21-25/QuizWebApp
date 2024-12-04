dfdc91241eda73a0be2a3504188faf100f250d44
hs223nr <hs223nr@student.lnu.se>
2024-12-04 14:59:13 +0100

    fix entry and vite config to run

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-export default {
-  root: 'src',
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './src', // Ensures Vite looks for `src/index.html`
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    target: 'esnext'
+    outDir: '../dist', // Outputs build files to `dist/`
+    emptyOutDir: true
+  },
+  server: {
+    open: true // Automatically open the app in the browser
   }
-}
+});
